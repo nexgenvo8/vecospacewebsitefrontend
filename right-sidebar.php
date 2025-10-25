430b9d93a79d348c931148207bbd0189eee342a1
Shadab <mohd.shadab@corrintech.com>
2025-10-25 17:13:44 +0530

    alumniSecondcodeuploaded

--- right-sidebar.php
+++ right-sidebar.php
@@ -314,10 +314,10 @@ if ($jcarousellite != 1) { ?>
 	<div id="trend">
 
 
-		<?php if ($_SESSION["sessUserId"] != '' && $_SESSION["sessUserId"] != 0) { ?>
+		<?php if (isset($_SESSION['sessUserId']) && $_SESSION["sessUserId"] != '' && $_SESSION["sessUserId"] != 0) { ?>
 			<div class="right-fttr">
 				<ul class="fttr-list">
-					<li><a href="<?php echo $fullurl; ?>privacy.html" target="_blank">Privacy</a></li>
+					<li><a href="<?php echo $fullurl; ?>alumni-pvc.html" target="_blank">Privacy</a></li>
 					<li><a href="<?php echo $fullurl; ?>terms.html" target="_blank">Terms</a></li>
 					<li><a href="<?php echo $fullurl; ?>about.html" target="_blank">About</a></li>
 					<li><a href="<?php echo $fullurl; ?>faq.html" target="_blank">FAQ's</a></li>
